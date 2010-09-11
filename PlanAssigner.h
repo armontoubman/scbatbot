59275159dda0aba80da5f864deeb794f89390366
armontoubman <armontoubman@a850bb65-3c1a-43af-a1ba-18f3eb5da290>
2010-09-11 11:37:03 +0000

    delete planmanager, add planassigner

---
+++ PlanAssigner.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <BWAPI.h>
+#include "HighCommand.h"
+#include "TaskManager.h"
+#include "EigenUnitGroupManager.h"
+#include "EnemyUnitDataManager.h"
+#include "MicroManager.h"
+class HighCommand;
+class TaskManager;
+class EigenUnitGroupManager;
+class EnemyUnitDataManager;
+class MicroManager;
+class PlanAssigner
+{
+public:
+	PlanAssigner(HighCommand* h, TaskManager* t, EigenUnitGroupManager* e, EnemyUnitDataManager* ed, MicroManager* m);
+
+	HighCommand* hc;
+	TaskManager* tm;
+	EigenUnitGroupManager* eiugm;
+	EnemyUnitDataManager* eudm;
+	MicroManager* mm;
+
+	std::map<UnitGroup*, Task> maakPlan();
+	int canAttack(UnitGroup* ug1, UnitGroup* ug2);
+	Task mostAppropriate(UnitGroup* current, int tasktype, std::map<UnitGroup*, Task> currentPlan);
+	Task mostAppropriate(UnitGroup* current, int tasktype, std::map<UnitGroup*, Task> currentPlan, bool nullwaarde);
+	bool containsTask(std::map<UnitGroup*, Task> plan, Task t);
+	void addAssignment(UnitGroup* ug, Task t, std::map<UnitGroup*, Task>* plan);
+	double logicaldistance(UnitGroup* ug, BWAPI::Position pos);
+};
+
